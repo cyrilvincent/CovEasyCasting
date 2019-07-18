72570494f4755ef7e3125a29112d7fb9d0e8c6e0
cyrilvincent <contact@cyrilvincent.com>
2019-07-18 13:20:41 +0200

    MockSocket

---
+++ mocksocketserver50.py
@@ -0,0 +1,31 @@
+port = 50
+print(f"Mock BT by Socket Test {port}")
+print("=========================")
+
+import uuid
+mac = uuid.getnode()
+mac = ':'.join(("%012X" % mac)[i:i+2] for i in range(0, 12, 2))
+print(f"Starting BT Server {mac}[{port}]...")
+
+import socket
+print(socket.AF_BLUETOOTH)
+sock = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
+sock.bind(("",port))
+sock.listen(1)
+
+print("Waiting for connection...")
+clientSock, clientInfo = sock.accept()
+print(f"Accepted connection from {clientInfo}")
+import time
+try:
+    i = 1
+    while i<3600:
+        clientSock.send(i)
+        i += 1
+        time.sleep(1)
+except IOError as ex:
+    print(f"Error: {ex}")
+
+clientSock.close()
+sock.close()
+
