f564c197c8c7e64b25d5f6667da49309b2da331e
cyrilvincent <contact@cyrilvincent.com>
2019-09-13 23:15:23 +0200

    autostart echo

--- setup/autostart.sh
+++ setup/autostart.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
+echo AutoStart PiBox
 cd /home/pi/pibox
 sleep 1
+echo PiBox update
 git pull
 sleep 1
+echo Kill previous version
 pkill -9 python3
 sleep 2
+echo Starting
 python3 consolepibox.py
