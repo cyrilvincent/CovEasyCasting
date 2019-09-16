7d82927482fc05be857b9986287b68ec68bef815
cyrilvincent <contact@cyrilvincent.com>
2019-09-16 21:56:23 +0200

    Improve pibox.service for log &>

--- setup/autostart.sh
+++ setup/autostart.sh
@@ -5,7 +5,7 @@
 #sudo systemctl stop pibox.service
 #To enable the service
 #sudo systemctl enable pibox.service
-cp pibox.log pibox.bak
+cp /home/pi/pibox.log /home/pi/pibox.bak
 echo AutoStart PiBox > /home/pi/pibox.log
 echo Kill previous version >> /home/pi/pibox.log
 pkill -9 python3 >> /home/pi/pibox.log
@@ -16,4 +16,4 @@ echo PiBox update >> /home/pi/pibox.log
 git pull >> /home/pi/pibox.log
 sleep 1
 echo Starting PiBox >> /home/pi/pibox.log
-python3 consolepibox.py 2>> /home/pi/pibox.log
+python3 consolepibox.py &>> /home/pi/pibox.log
