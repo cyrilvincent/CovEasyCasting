8078a9263044b666517fbc5822b3104decb44f40
cyrilvincent <contact@cyrilvincent.com>
2019-09-27 17:24:29 +0200

    Service start

--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 #cp /home/pi/pibox/scripts/config/lxsession/LXDE-pi/autostart /home/pi/.config/lxsession/LXDE-pi/autostart
-#or
-#sudo cp pibox.service /etc/systemd/system/pibox.service
-#sudo systemctl daemon-reload
-#sudo systemctl start pibox.service
-#sudo systemctl stop pibox.service
-#To enable the service
-#sudo systemctl enable pibox.service
 cd /home/pi/pibox
 mv ../pibox.log ../pibox.bak
 git pull > ../pibox.log
---
+++ scripts/servicestart.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#sudo cp pibox.service /etc/systemd/system/pibox.service
+#sudo systemctl daemon-reload
+#sudo systemctl start pibox.service
+#sudo systemctl stop pibox.service
+#To enable the service
+#sudo systemctl enable pibox.service
+cd /home/pi/pibox
+echo start > ../servicestart.pibox
+mv ../pibox.log ../pibox.bak
+git pull > ../pibox.log
+screen -S pibox ./btpibox.py
